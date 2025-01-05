3839e9c6b014bb8730b0f062bc147edbe962141c
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-01-05 19:59:20 +0900

    chore/#1: 아이콘 및 이미지 추가

---
+++ src/assets/common/index.ts
@@ -0,0 +1,2 @@
+export { default as PrevIcon } from './ic_prev.svg';
+export { default as YogerLogoIcon } from './ic_yoger_logo.svg';
---
+++ src/assets/login/index.js
@@ -0,0 +1,3 @@
+export { default as GoogleCircleImg } from './img_google_circle.svg';
+export { default as KakaoCircleImg } from './img_kakao_circle.svg';
+export { default as NaverCircleImg } from './img_naver_circle.svg';
