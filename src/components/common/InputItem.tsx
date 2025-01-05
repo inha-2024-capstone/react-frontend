f332adb8453115c6e6306089577a4f49fbe47dbf
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-01-05 20:08:47 +0900

    design/#1: InputItem 컴포넌트

---
+++ src/components/common/InputItem.tsx
@@ -0,0 +1,52 @@
+import styled from 'styled-components';
+
+interface InputItemProps {
+  label: string;
+  type: string;
+  value: string | number;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  placeholder: string;
+}
+
+const InputItem: React.FC<InputItemProps> = ({
+  label,
+  type,
+  value,
+  onChange,
+  placeholder,
+}) => {
+  return (
+    <SignUpLabel>
+      {label}
+      <SignUpInput
+        type={type}
+        value={value}
+        onChange={onChange}
+        placeholder={placeholder}
+      />
+    </SignUpLabel>
+  );
+};
+
+export default InputItem;
+
+const SignUpLabel = styled.label`
+  display: block;
+  width: 100%;
+  margin-top: 1rem;
+  font-size: 0.8rem;
+  font-weight: 500;
+`;
+const SignUpInput = styled.input`
+  display: block;
+  width: 100%;
+  margin-top: 0.4rem;
+  padding: 0.5rem 0.7rem;
+  border: 1px solid #858b95;
+  border-radius: 0.5rem;
+  font-size: 0.8rem;
+
+  &:focus-visible {
+    outline: 1px solid #1e3050;
+  }
+`;
