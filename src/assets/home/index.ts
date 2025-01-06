08c30db1ce5be1485ec17b108eec5378c6f8dd11
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-01-06 20:13:18 +0900

    chore/#4: 아이콘, 이미지 추가

---
+++ src/assets/home/index.ts
@@ -0,0 +1,7 @@
+export { default as HeroImg } from './img_hero.svg';
+export { default as ShowerProductImg } from './img_shower_product.svg';
+export { default as LifeProductImg } from './img_life_product.svg';
+export { default as ShirtProductImg } from './img_shirt_product.svg';
+export { default as TeaProductImg } from './img_tea_product.svg';
+export { default as DivideTwoIcon } from './ic_divide_two.svg';
+export { default as DivideThreeIcon } from './ic_divide_three.svg';
