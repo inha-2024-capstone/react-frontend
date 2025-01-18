ce69d14864fb74260c191c170fbc74bdd328e0a3
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-01-18 20:31:32 +0900

    feat/#12: 마이페이지에 회원 정보 반영

---
+++ src/components/my/My.tsx
@@ -0,0 +1,164 @@
+import { useNavigate } from 'react-router-dom';
+import UserService from '../../services/UserService';
+import { useAuthStore, useUserInfoStore } from '../../store/store';
+import styled from 'styled-components';
+import { useEffect, useState } from 'react';
+
+const My: React.FC = () => {
+  const navigate = useNavigate();
+  const [isLoaded, setIsLoaded] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (useUserInfoStore.getState().nickName === '') {
+      setIsLoaded(true);
+      UserService.getAuthInfo().then((response) => {
+        if (response.isSucceeded === false) {
+          alert('유저 정보를 가져오는데 실패했습니다.');
+          return;
+        }
+        console.log('auth info', response);
+        useUserInfoStore.getState().setUserInfo(response.result);
+        setIsLoaded(false);
+      });
+    }
+  }, []);
+
+  return (
+    <>
+      {isLoaded ? (
+        <LoadingSection>
+          <LoadingTitle>로딩 중</LoadingTitle>
+          <LoadingText>유저 정보를 불러오는 중입니다</LoadingText>
+        </LoadingSection>
+      ) : (
+        <MyMain>
+          <MyTitle>My Page</MyTitle>
+          <MainBackground>
+            <MainSection>
+              <MyTitle>My Info</MyTitle>
+              <MyImg src={useUserInfoStore.getState().imageUri} alt="profile" />
+
+              <MainInfo>
+                <MyNickname>{useUserInfoStore.getState().nickName}</MyNickname>
+                <MyEmail>Email : {useUserInfoStore.getState().email}</MyEmail>
+              </MainInfo>
+            </MainSection>
+          </MainBackground>
+
+          <UserDetail>
+            <UserDetailTitle>세부 정보</UserDetailTitle>
+            <DetailItem
+              title="이름"
+              content={useUserInfoStore.getState().username}
+            />
+            <DetailItem
+              title="전화번호"
+              content={useUserInfoStore.getState().phoneNumber}
+            />
+            <DetailItem
+              title="성별"
+              content={
+                useUserInfoStore.getState().gender == 'MALE' ? '남자' : '여자'
+              }
+            />
+            <DetailItem
+              title="주소"
+              content={useUserInfoStore.getState().address}
+            />
+          </UserDetail>
+        </MyMain>
+      )}
+    </>
+  );
+};
+
+export default My;
+
+const DetailItem: React.FC<{ title: string; content: string }> = ({
+  title,
+  content,
+}) => {
+  return (
+    <UserDetailItem>
+      <UserDetailItemTitle>{title}</UserDetailItemTitle>
+      <UserDetailItemContent>{content}</UserDetailItemContent>
+    </UserDetailItem>
+  );
+};
+
+const MyMain = styled.main`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  flex: 1;
+  min-height: calc(100vh - 14rem);
+`;
+const MyTitle = styled.h2`
+  display: none;
+`;
+const MainBackground = styled.div`
+  width: 100%;
+  background-color: #9f9f9f;
+  padding: 1.5rem 1rem;
+`;
+const MainSection = styled.section`
+  display: flex;
+  align-items: center;
+  background-color: #fff;
+  border-radius: 12px;
+  padding: 1rem 0.7rem;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+`;
+const MyImg = styled.img`
+  width: 5rem;
+  margin-right: 0.5rem;
+  padding-bottom: 0.2rem;
+  border-radius: 100%;
+`;
+const MainInfo = styled.div``;
+const MyNickname = styled.h3`
+  margin: 0;
+  font-size: 1.5rem;
+`;
+const MyEmail = styled.p`
+  font-size: 0.8rem;
+  margin: 0;
+  margin-top: 0.3rem;
+`;
+const UserDetail = styled.section`
+  padding: 1rem;
+`;
+const UserDetailTitle = styled.h3`
+  margin-bottom: 0.5rem;
+`;
+const UserDetailItem = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const UserDetailItemTitle = styled.h4`
+  margin: 0;
+  width: 5rem;
+  font-size: 0.9rem;
+`;
+const UserDetailItemContent = styled.p`
+  flex: 1;
+  font-size: 0.9rem;
+`;
+// 로딩 화면
+const LoadingSection = styled.section`
+  flex: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-weight: 600;
+  color: #1e3050;
+  background-color: #f1f1f1;
+`;
+const LoadingTitle = styled.h2`
+  margin: 0;
+  font-size: 1.2rem;
+`;
+const LoadingText = styled.p`
+  font-size: 0.8rem;
+`;
