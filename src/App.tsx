18c17e3d542b9d616019ff38477a63710ad6478c
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-01-18 10:44:13 +0900

    feat/#3: 컴포넌트 마운트 시 토큰 갱신

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import {
   BrowserRouter as Router,
   Route,
   Routes,
   Navigate,
+  useNavigate,
 } from 'react-router-dom';
 import { useAuthStore } from './store/store';
 import { LoginPage, SignUpPage, HomePage, ProductPage } from './pages';
 import ProductRegisterPage from './pages/ProductRegisterPage';
+import UserService from './services/UserService';
 
 const MyPage: React.FC = () => {
   return <h1>My Page</h1>;
@@ -15,6 +17,29 @@ const MyPage: React.FC = () => {
 
 const App: React.FC = () => {
   const isAuth = useAuthStore((state) => state.isAuth);
+
+  useEffect(() => {
+    const accessToken = localStorage.getItem('yogerAccessToken');
+    const refreshToken = localStorage.getItem('yogerRefreshToken');
+    if (accessToken && refreshToken) {
+      UserService.refreshToken()
+        .then((response) => {
+          if (response.isSucceeded === false) {
+            localStorage.removeItem('yogerAccessToken');
+            localStorage.removeItem('yogerRefreshToken');
+            useAuthStore.getState().logout();
+            return;
+          }
+          console.log('refresh token');
+          useAuthStore
+            .getState()
+            .login(response.accessToken, response.refreshToken);
+        })
+        .catch((error) => {
+          console.log('error', error);
+        });
+    }
+  }, []);
 
   return (
     <Router>
