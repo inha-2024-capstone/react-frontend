8a7e2f9b9dbfaec6df67c48e8158f0f652162c3d
CHO EUN JUNG <0516dmswjd@gmail.com>
2025-01-18 14:40:56 +0900

    feat/#3: 모달 컴포넌트 구현

---
+++ src/components/common/ModalComponent.tsx
@@ -0,0 +1,52 @@
+import Modal from 'react-modal';
+
+Modal.setAppElement('#root');
+
+interface ModalProps {
+  isOpen: boolean;
+  setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  contentLabel: string;
+  children: React.ReactNode;
+}
+
+const ModalComponent: React.FC<ModalProps> = ({
+  isOpen,
+  setIsOpen,
+  contentLabel,
+  children,
+}) => {
+  return (
+    <Modal
+      isOpen={isOpen}
+      contentLabel={contentLabel}
+      style={customStyles}
+      onRequestClose={() => setIsOpen(false)}
+    >
+      {children}
+    </Modal>
+  );
+};
+
+export default ModalComponent;
+
+const customStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 1000,
+  },
+  content: {
+    display: 'flex',
+    flexDirection: 'column' as 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: '80%',
+    maxWidth: '480px',
+    height: 'max-content',
+    maxHeight: '80%',
+    padding: 0,
+    margin: 'auto',
+    backgroundColor: 'white',
+    borderRadius: '0.5rem',
+    zIndex: 1000,
+  },
+};
